e4f9a5e9fb7abec3bb79b49002cfc0b132d83a8e
h-dr4g0n <phamhiep2506@gmail.com>
2020-02-08 15:54:35 +0700

    fix error

--- termux-setup.sh
+++ termux-setup.sh
@@ -2,14 +2,14 @@
 #Termux upgrade package ...
 pkg upgrade -y
 #Termux install package ...
-pkg install zsh curl git ruby cowsay -y
+pkg install zsh curl git ruby vim cowsay -y
 #install lolcat ...
 gem install lolcat
 #install and setup basic oh-my-zsh ...
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #set theme oh-my-zsh
-sed -i -e "s/robbyrussell/agnoster/g" ~/.zshrc
-cp ~/termux-setup-basic/fonts/font.ttf ~/.termux/
+#sed -i -e "s/robbyrussell/agnoster/g" ~/.zshrc
+#cp ~/termux-setup-basic/fonts/font.ttf ~/.termux/
 
 #touch keyboard
 #mkdir ~/.termux
