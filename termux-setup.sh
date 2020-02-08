d164a916441c44c8e4b4afb0e45acd6ae58550fd
h-dr4g0n <33449999+h-dr4g0n@users.noreply.github.com>
2020-02-08 09:18:35 +0700

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -9,7 +9,13 @@ gem install lolcat
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #set theme oh-my-zsh
 sed -i -e "s/robbyrussell/agnoster/g" ~/.zshrc
-cp termux-setup-basic/fonts/font.ttf ~/.termux/
+cp ~/termux-setup-basic/fonts/font.ttf ~/.termux/
+
+#touch keyboard
+mkdir .termux
+touch termux.properties
+echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" > ~/.termux/termux.properties
+
 #edit banner
 echo "clear" >> $PREFIX/etc/zshrc
 
